6ee4e2d7ccac937e16054694a528d83ad0d26577
maegan's <153042080+maegan08099800@users.noreply.github.com>
2024-08-15 11:55:46 +0800

    Update custom_appbar.dart

--- lib/custom_widgets/custom_appbar.dart
+++ lib/custom_widgets/custom_appbar.dart
@@ -1,69 +1,65 @@
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:luvpark_get/custom_widgets/custom_text.dart';
 
 class CustomAppbar extends StatelessWidget {
-  final Function onTap;
+  final Function? onTap;
   final String title;
-  final Widget? action;
+  final List<Widget>? action;
   final Color? bgColor;
-  const CustomAppbar(
-      {super.key,
-      required this.onTap,
-      required this.title,
-      required this.action,
-      this.bgColor});
+  const CustomAppbar({
+    super.key,
+    this.onTap,
+    required this.title,
+    this.action,
+    this.bgColor,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: bgColor ?? Colors.white,
-      child: Row(
+    return AppBar(
+      centerTitle: true,
+      backgroundColor: bgColor ?? Colors.white,
+      systemOverlayStyle: const SystemUiOverlayStyle(
+        statusBarColor: Colors.white,
+        statusBarBrightness: Brightness.light,
+        statusBarIconBrightness: Brightness.dark,
+      ),
+      leading: Row(
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          Expanded(
-            child: Row(
-              children: [
-                IconButton(
-                  padding: EdgeInsets.zero,
-                  onPressed: () {
-                    onTap();
-                  },
-                  icon: const Icon(
-                    Icons.chevron_left,
-                    color: Colors.black,
-                  ),
-                ),
-                const CustomParagraph(
-                  text: "Back",
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                ),
-              ],
+          IconButton(
+            alignment: Alignment.center,
+            padding: EdgeInsets.zero,
+            onPressed: () {
+              //defaultBack
+              if (onTap == null) {
+                Get.back();
+                return;
+              }
+              onTap!();
+            },
+            icon: const Icon(
+              Icons.chevron_left,
+              color: Colors.black,
             ),
           ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.center,
-              child: CustomTitle(
-                text: title,
-                fontSize: 20,
-                fontWeight: FontWeight.w900,
-              ),
-            ),
-          ),
-          Expanded(
-            child: Align(
-              alignment: Alignment.centerRight,
-              child: Padding(
-                padding: EdgeInsets.only(right: 15),
-                child: action,
-              ),
-            ),
+          const CustomParagraph(
+            text: "Back",
+            fontSize: 16,
+            fontWeight: FontWeight.w600,
           ),
         ],
       ),
+      leadingWidth: 100,
+      title: CustomTitle(
+        text: title,
+        fontSize: 20,
+        fontWeight: FontWeight.w900,
+        color: Colors.black,
+      ),
+      actions: action,
     );
   }
 }
