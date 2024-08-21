cac79689630e4d53d0aadff2171500c8a0c3e07c
Kres12 <160553090+Kres12@users.noreply.github.com>
2024-08-21 11:32:39 +0800

    drawer update

--- lib/drawer/view.dart
+++ lib/drawer/view.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:luvpark_get/auth/authentication.dart';
 import 'package:luvpark_get/custom_widgets/alert_dialog.dart';
-import 'package:luvpark_get/custom_widgets/app_color.dart';
 import 'package:luvpark_get/custom_widgets/custom_text.dart';
 import 'package:luvpark_get/mapa/controller.dart';
 import 'package:luvpark_get/routes/routes.dart';
@@ -21,151 +20,240 @@ class CustomDrawer extends GetView<DashboardMapController> {
             child: ListView(
               padding: EdgeInsets.zero,
               children: <Widget>[
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                    color: AppColor.primaryColor,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Row(
-                          children: [
-                            CircleAvatar(
-                              radius: 24,
-                              backgroundColor: Colors.white,
-                              backgroundImage: controller.userProfile != null &&
-                                      controller.userProfile['image_base64'] !=
-                                          null
-                                  ? MemoryImage(
-                                      base64Decode(controller
-                                          .userProfile['image_base64']),
-                                    )
-                                  : null,
-                              child: controller.userProfile == null
-                                  ? const Icon(Icons.person,
-                                      size: 24, color: Colors.blueAccent)
-                                  : null,
-                            ),
-                            const SizedBox(width: 10),
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.center,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                CustomTitle(
-                                  text: controller.userProfile != null
-                                      ? '${controller.userProfile['first_name']} ${controller.userProfile['last_name']}'
-                                      : "Not Specified",
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                ),
-                                CustomParagraph(
-                                  text: controller.userProfile != null
-                                      ? controller.userProfile['email']
-                                      : "Email Address",
-                                  color: Colors.white70,
-                                  fontSize: 13,
-                                ),
-                              ],
-                            ),
-                          ],
-                        ),
-                      ],
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 67),
+                    CircleAvatar(
+                      radius: 44,
+                      backgroundColor: Colors.white,
+                      backgroundImage: controller.userProfile != null &&
+                              controller.userProfile['image_base64'] != null
+                          ? MemoryImage(
+                              base64Decode(
+                                  controller.userProfile['image_base64']),
+                            )
+                          : null,
+                      child: controller.userProfile == null
+                          ? const Icon(Icons.person,
+                              size: 44, color: Colors.blueAccent)
+                          : null,
                     ),
-                  ),
+                    Container(
+                      height: 10,
+                    ),
+                    CustomTitle(
+                      text: controller.userProfile != null
+                          ? '${controller.userProfile['first_name']} ${controller.userProfile['last_name']}'
+                          : "Not Specified",
+                      color: Colors.black,
+                      fontSize: 16,
+                      fontStyle: FontStyle.normal,
+                      textAlign: TextAlign.center,
+                      fontWeight: FontWeight.w700,
+                      letterSpacing: -0.408,
+                    ),
+                    Container(height: 10),
+                    TextButton(
+                      onPressed: () {},
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          const Color(0xFFEDEFF3),
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(58.0),
+                          ),
+                        ),
+                        padding: MaterialStateProperty.all<EdgeInsets>(
+                          const EdgeInsets.symmetric(horizontal: 20.0),
+                        ),
+                      ),
+                      child: const CustomTitle(
+                        text: "My Profile and Settings",
+                        fontSize: 14,
+                        fontStyle: FontStyle.normal,
+                        fontWeight: FontWeight.w700,
+                        textAlign: TextAlign.center,
+                        letterSpacing: -0.408,
+                      ),
+                    ),
+                    Container(height: 42),
+                  ],
                 ),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15.0),
                   child: CustomTitle(
-                      text: "My Account".toUpperCase(),
-                      letterSpacing: 0.1,
-                      fontWeight: FontWeight.w900,
-                      color: AppColor.primaryColor),
+                    text: "My Account",
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    letterSpacing: -0.408,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFFA1A1A9),
+                  ),
                 ),
                 ListTile(
-                  leading: const Icon(Icons.person, color: Colors.blueAccent),
-                  title: const CustomParagraph(text: "My Parking"),
+                  leading: const Icon(Icons.person, color: Color(0xFF1C1C1E)),
+                  title: const CustomTitle(
+                    text: "My Parking",
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -0.408,
+                    color: Color(0xFF1C1C1E),
+                  ),
+                  trailing: const Icon(Icons.chevron_right_sharp,
+                      color: Color(0xFF1C1C1E)),
                   onTap: () {
                     Get.offAndToNamed(Routes.parking);
                   },
                 ),
                 ListTile(
-                  leading: const Icon(Icons.wallet, color: Colors.blueAccent),
-                  title: const CustomParagraph(text: "Wallet"),
+                  leading: const Icon(Icons.wallet, color: Color(0xFF1C1C1E)),
+                  title: const CustomTitle(
+                    text: "Wallet",
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -0.408,
+                    color: Color(0xFF1C1C1E),
+                  ),
+                  trailing: const Icon(Icons.chevron_right_sharp,
+                      color: Color(0xFF1C1C1E)),
                   onTap: () {
                     Get.toNamed(Routes.wallet);
                   },
                 ),
                 ListTile(
-                  leading: const Icon(Icons.settings, color: Colors.blueAccent),
-                  title: const CustomParagraph(text: "Settings"),
+                  leading: const Icon(Icons.settings, color: Color(0xFF1C1C1E)),
+                  title: const CustomTitle(
+                    text: "Settings",
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -0.408,
+                    color: Color(0xFF1C1C1E),
+                  ),
+                  trailing: const Icon(Icons.chevron_right_sharp,
+                      color: Color(0xFF1C1C1E)),
                   onTap: () {
                     Navigator.pop(context);
                   },
                 ),
-                const Divider(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                Container(height: 22),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 15.0),
                   child: CustomTitle(
-                      text: "Help and Support".toUpperCase(),
-                      letterSpacing: 0.1,
-                      fontWeight: FontWeight.w900,
-                      color: AppColor.primaryColor),
+                    text: "Help and Support",
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    letterSpacing: -0.408,
+                    fontWeight: FontWeight.w700,
+                    color: Color(0xFFA1A1A9),
+                  ),
                 ),
                 ListTile(
-                  leading: const Icon(Icons.person, color: Colors.blueAccent),
-                  title: const CustomParagraph(text: "FAQs"),
+                  leading: const Icon(Icons.person, color: Color(0xFF1C1C1E)),
+                  title: const CustomTitle(
+                    text: "FAQs",
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -0.408,
+                    color: Color(0xFF1C1C1E),
+                  ),
+                  trailing: const Icon(Icons.chevron_right_sharp,
+                      color: Color(0xFF1C1C1E)),
                   onTap: () {
                     Get.offAndToNamed(Routes.faqpage);
                   },
                 ),
                 ListTile(
-                  leading: const Icon(Icons.wallet, color: Colors.blueAccent),
-                  title: const CustomParagraph(text: "Terms of Use"),
+                  leading: const Icon(Icons.settings, color: Color(0xFF1C1C1E)),
+                  title: const CustomTitle(
+                    text: "Terms of Use",
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -0.408,
+                    color: Color(0xFF1C1C1E),
+                  ),
+                  trailing: const Icon(Icons.chevron_right_sharp,
+                      color: Color(0xFF1C1C1E)),
                   onTap: () {
                     Get.toNamed(Routes.wallet);
                   },
                 ),
                 ListTile(
-                  leading: const Icon(Icons.settings, color: Colors.blueAccent),
-                  title: const CustomParagraph(text: "Privacy Policy"),
+                  leading: const Icon(Icons.settings, color: Color(0xFF1C1C1E)),
+                  title: const CustomTitle(
+                    text: "Privacy Policy",
+                    fontSize: 14,
+                    fontStyle: FontStyle.normal,
+                    fontWeight: FontWeight.w700,
+                    letterSpacing: -0.408,
+                    color: Color(0xFF1C1C1E),
+                  ),
+                  trailing: const Icon(Icons.chevron_right_sharp,
+                      color: Color(0xFF1C1C1E)),
                   onTap: () {
                     Navigator.pop(context);
                   },
                 ),
-                const Divider(),
               ],
             ),
           ),
-          const Divider(),
-          ListTile(
-            leading: const Icon(Icons.logout, color: Colors.red),
-            title: const CustomParagraph(
-              text: "Logout",
-              color: Colors.red,
-            ),
-            onTap: () async {
-              CustomDialog().confirmationDialog(context, "Logout",
-                  "Are you sure you want to logout?", "No", "Yes", () {
-                Get.back();
-              }, () async {
-                Get.back();
-                CustomDialog().loadingDialog(context);
-                await Future.delayed(const Duration(seconds: 3));
-                final userLogin = await Authentication().getUserLogin();
-                List userData = [userLogin];
-                userData = userData.map((e) {
-                  e["is_login"] = "N";
-                  return e;
-                }).toList();
+          Padding(
+            padding: const EdgeInsets.only(bottom: 35, left: 60, right: 60),
+            child: SizedBox(
+              width: 200,
+              child: TextButton(
+                onPressed: () {
+                  CustomDialog().confirmationDialog(context, "Logout",
+                      "Are you sure you want to logout?", "No", "Yes", () {
+                    Get.back();
+                  }, () async {
+                    Get.back();
+                    CustomDialog().loadingDialog(context);
+                    await Future.delayed(const Duration(seconds: 3));
+                    final userLogin = await Authentication().getUserLogin();
+                    List userData = [userLogin];
+                    userData = userData.map((e) {
+                      e["is_login"] = "N";
+                      return e;
+                    }).toList();
 
-                await Authentication().setLogin(jsonEncode(userData[0]));
-                Get.back();
-                Get.offAllNamed(Routes.splash);
-              });
-            },
+                    await Authentication().setLogin(jsonEncode(userData[0]));
+                    Get.back();
+                    Get.offAllNamed(Routes.splash);
+                  });
+                },
+                style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all<Color>(
+                    const Color(0xFFBD2424).withOpacity(0.12),
+                  ),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(58.0),
+                    ),
+                  ),
+                  padding: MaterialStateProperty.all<EdgeInsets>(
+                    const EdgeInsets.symmetric(horizontal: 20.0),
+                  ),
+                ),
+                child: const CustomTitle(
+                  text: "Log out",
+                  fontSize: 14,
+                  color: Color(0xFFBD2424),
+                  fontStyle: FontStyle.normal,
+                  fontWeight: FontWeight.w700,
+                  textAlign: TextAlign.center,
+                  letterSpacing: -0.408,
+                ),
+              ),
+            ),
           ),
         ],
       ),
--- lib/faq/view.dart
+++ lib/faq/view.dart
@@ -1,4 +1,3 @@
-import 'package:flip_card/flip_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:get/get.dart';
@@ -47,94 +46,94 @@ class FaqPage extends GetView<FaqPageController> {
               fontSize: 20,
               letterSpacing: 0.0,
             ),
-            Expanded(
-              child: GridView.builder(
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 2,
-                  crossAxisSpacing: 10.0,
-                  mainAxisSpacing: 10.0,
-                ),
-                itemCount: controller.faqsData.length,
-                itemBuilder: (context, index) {
-                  return FlipCard(
-                    onFlip: () async {
-                      if (!controller.faqsData[index].containsKey('answers')) {
-                        var faqId = controller.faqsData[index]['id'];
-                        await controller.getFaqAnswers(faqId, index);
-                      }
-                    },
-                    front: Container(
-                      decoration: BoxDecoration(
-                        color: AppColor.primaryColor,
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: const Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: CustomParagraph(
-                              text: controller.faqsData[index]['faq_text'],
-                              fontWeight: FontWeight.bold,
-                              color: Colors.white,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
-                    back: Container(
-                      decoration: BoxDecoration(
-                        color: AppColor.primaryColor,
-                        borderRadius: BorderRadius.circular(15),
-                        boxShadow: [
-                          BoxShadow(
-                            color: Colors.black.withOpacity(0.3),
-                            spreadRadius: 2,
-                            blurRadius: 5,
-                            offset: const Offset(0, 3),
-                          ),
-                        ],
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: controller.faqsData[index]
-                                .containsKey('answers')
-                            ? (controller.faqsData[index]['answers'] as List)
-                                .map((answer) {
-                                return Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: CustomParagraph(
-                                    text: answer[
-                                        'faq_ans_text'], // Adjust this key based on your API response
-                                    color: Colors.white,
-                                    textAlign: TextAlign.start,
-                                  ),
-                                );
-                              }).toList()
-                            : [
-                                const CustomParagraph(
-                                  text: 'Loading...',
-                                  color: Colors.white,
-                                  textAlign: TextAlign.center,
-                                ),
-                              ],
-                      ),
-                    ),
-                  );
-                },
-              ),
-            )
+            // Expanded(
+            //   child: GridView.builder(
+            //     gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            //       crossAxisCount: 2,
+            //       crossAxisSpacing: 10.0,
+            //       mainAxisSpacing: 10.0,
+            //     ),
+            //     itemCount: controller.faqsData.length,
+            //     itemBuilder: (context, index) {
+            //       return FlipCard(
+            //         onFlip: () async {
+            //           if (!controller.faqsData[index].containsKey('answers')) {
+            //             var faqId = controller.faqsData[index]['id'];
+            //             await controller.getFaqAnswers(faqId, index);
+            //           }
+            //         },
+            //         front: Container(
+            //           decoration: BoxDecoration(
+            //             color: AppColor.primaryColor,
+            //             borderRadius: BorderRadius.circular(15),
+            //             boxShadow: [
+            //               BoxShadow(
+            //                 color: Colors.black.withOpacity(0.3),
+            //                 spreadRadius: 2,
+            //                 blurRadius: 5,
+            //                 offset: const Offset(0, 3),
+            //               ),
+            //             ],
+            //           ),
+            //           child: Column(
+            //             mainAxisAlignment: MainAxisAlignment.center,
+            //             crossAxisAlignment: CrossAxisAlignment.center,
+            //             children: [
+            //               Padding(
+            //                 padding: const EdgeInsets.all(8.0),
+            //                 child: CustomParagraph(
+            //                   text: controller.faqsData[index]['faq_text'],
+            //                   fontWeight: FontWeight.bold,
+            //                   color: Colors.white,
+            //                   textAlign: TextAlign.center,
+            //                 ),
+            //               ),
+            //             ],
+            //           ),
+            //         ),
+            //         back: Container(
+            //           decoration: BoxDecoration(
+            //             color: AppColor.primaryColor,
+            //             borderRadius: BorderRadius.circular(15),
+            //             boxShadow: [
+            //               BoxShadow(
+            //                 color: Colors.black.withOpacity(0.3),
+            //                 spreadRadius: 2,
+            //                 blurRadius: 5,
+            //                 offset: const Offset(0, 3),
+            //               ),
+            //             ],
+            //           ),
+            //           child: Column(
+            //             mainAxisAlignment: MainAxisAlignment.center,
+            //             crossAxisAlignment: CrossAxisAlignment.start,
+            //             children: controller.faqsData[index]
+            //                     .containsKey('answers')
+            //                 ? (controller.faqsData[index]['answers'] as List)
+            //                     .map((answer) {
+            //                     return Padding(
+            //                       padding: const EdgeInsets.all(8.0),
+            //                       child: CustomParagraph(
+            //                         text: answer[
+            //                             'faq_ans_text'], // Adjust this key based on your API response
+            //                         color: Colors.white,
+            //                         textAlign: TextAlign.start,
+            //                       ),
+            //                     );
+            //                   }).toList()
+            //                 : [
+            //                     const CustomParagraph(
+            //                       text: 'Loading...',
+            //                       color: Colors.white,
+            //                       textAlign: TextAlign.center,
+            //                     ),
+            //                   ],
+            //           ),
+            //         ),
+            //       );
+            //     },
+            //   ),
+            // )
           ],
         ),
       ),
