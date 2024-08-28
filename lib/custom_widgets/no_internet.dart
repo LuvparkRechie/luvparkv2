7df84820894db04ac8e29de60aa14d3b945e946b
maegan's <153042080+maegan08099800@users.noreply.github.com>
2024-08-28 17:51:37 +0800

    No internet

--- lib/custom_widgets/no_internet.dart
+++ lib/custom_widgets/no_internet.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:luvpark_get/custom_widgets/custom_button.dart';
 import 'package:luvpark_get/custom_widgets/custom_text.dart';
 
