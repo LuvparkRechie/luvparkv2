79396295f42584b3778bde765ab263e8c9494bc6
Kres12 <160553090+Kres12@users.noreply.github.com>
2024-09-05 17:24:25 +0800

    transferred my vehicles to account page instead of drawer

--- lib/drawer/view.dart
+++ lib/drawer/view.dart
@@ -171,23 +171,23 @@ class CustomDrawer extends GetView<DashboardMapController> {
                     Get.toNamed(Routes.parking);
                   },
                 ),
-                ListTile(
-                  leading:
-                      SvgPicture.asset('assets/drawer_icon/drawer_parking.svg'),
-                  title: const CustomTitle(
-                    text: "My Vehicles",
-                    fontSize: 14,
-                    fontStyle: FontStyle.normal,
-                    fontWeight: FontWeight.w700,
-                    letterSpacing: -0.408,
-                    color: Color(0xFF1C1C1E),
-                  ),
-                  trailing: Icon(Icons.chevron_right_sharp,
-                      color: AppColor.primaryColor),
-                  onTap: () {
-                    Get.toNamed(Routes.myVehicles);
-                  },
-                ),
+                // ListTile(
+                //   leading:
+                //       SvgPicture.asset('assets/drawer_icon/drawer_parking.svg'),
+                //   title: const CustomTitle(
+                //     text: "My Vehicles",
+                //     fontSize: 14,
+                //     fontStyle: FontStyle.normal,
+                //     fontWeight: FontWeight.w700,
+                //     letterSpacing: -0.408,
+                //     color: Color(0xFF1C1C1E),
+                //   ),
+                //   trailing: Icon(Icons.chevron_right_sharp,
+                //       color: AppColor.primaryColor),
+                //   onTap: () {
+                //     Get.toNamed(Routes.myVehicles);
+                //   },
+                // ),
                 ListTile(
                   leading:
                       SvgPicture.asset('assets/drawer_icon/drawer_wallet.svg'),
