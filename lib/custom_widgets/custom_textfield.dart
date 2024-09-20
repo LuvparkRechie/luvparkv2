2ce3f47b3c471580004f3ef4d6cc3a81cd845715
CMDS-Rechie <155434447+LuvparkRechie@users.noreply.github.com>
2024-09-20 17:01:10 +0800

    mobile number formatter

--- lib/custom_widgets/custom_textfield.dart
+++ lib/custom_widgets/custom_textfield.dart
@@ -8,6 +8,7 @@ import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:luvpark_get/custom_widgets/app_color.dart';
 import 'package:luvpark_get/custom_widgets/custom_text.dart';
+import 'package:luvpark_get/custom_widgets/variables.dart';
 
 class CustomTextField extends StatefulWidget {
   final String? labelText;
@@ -162,7 +163,7 @@ class _CustomMobileNumberState extends State<CustomMobileNumber> {
       padding: const EdgeInsets.only(top: 10.0, bottom: 10),
       child: TextFormField(
         autofocus: false,
-        inputFormatters: Variables.maskFormatter,
+        inputFormatters: [Variables.maskFormatter],
         controller: widget.controller,
         textInputAction: TextInputAction.done,
         readOnly: !widget.isEnabled || widget.isReadOnly!,
